1787736aa79d0bcb2d39d8cb071edc98e5fff972
Michael Silva <silva.michael2013@gmail.com>
2023-11-11 19:47:56 -0300

    Página index implementada como como home, retirada dos scripts redundantes.

--- view/templates/products/products.js
+++ view/templates/products/products.js
@@ -1,38 +1,4 @@
 
-//Menu e Rodapé
-
-function carregarElementos() {
-    // Carregar cabeçalho
-    var xhrHeader = new XMLHttpRequest();
-    xhrHeader.open("GET", "/index.html", true);
-    xhrHeader.onreadystatechange = function () {
-        if (xhrHeader.readyState == 4 && xhrHeader.status == 200) {
-            document.querySelector(".header-content").innerHTML = xhrHeader.responseText;
-        }
-    };
-    xhrHeader.send();
-
-    // Carregar rodapé
-    var xhrFooter = new XMLHttpRequest();
-    xhrFooter.open("GET", "/index-footer.html", true);
-    xhrFooter.onreadystatechange = function () {
-        if (xhrFooter.readyState == 4 && xhrFooter.status == 200) {
-            document.querySelector(".footer").innerHTML = xhrFooter.responseText;
-        }
-    };
-    xhrFooter.send();
-
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    carregarElementos();
-});
-
-
-//Fim do Menu e Rodapé
-
-
-
 //filtro
 
 document.addEventListener("DOMContentLoaded", function () {
